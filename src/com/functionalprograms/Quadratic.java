5aa87c511aed71a1f8d16580b6206fc28c6df5e7
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-07 22:53:37 +0530

    added equations for root1,root2 and delta

--- src/com/functionalprograms/Quadratic.java
+++ src/com/functionalprograms/Quadratic.java
@@ -13,6 +13,14 @@ public class Quadratic {
 		int c=scan.nextInt();
 		
 		System.out.println("The quadratic equation is  : ("+a+")x^2+("+b+")x+("+c+")");
+		
+		int delta=b*b-4*a*c;
+		double root1=(-b+Math.sqrt(delta))/(2*a);
+		double root2=(-b-Math.sqrt(delta))/(2*a);
+		System.out.println("The root1 : "+root1);
+		System.out.println("The root2 : "+root2);
+		
+		
 	}
 
 }
